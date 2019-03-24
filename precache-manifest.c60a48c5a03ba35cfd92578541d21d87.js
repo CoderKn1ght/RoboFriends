bd5223e0ad30739d5fc1c62a80a45fd49608380a
Shashank Gaikaiwari <shashank.gaikaiwari@gmail.com>
2019-03-24 15:12:35 -0500

    Updates

---
+++ precache-manifest.c60a48c5a03ba35cfd92578541d21d87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5b0434e6d789f6cf73a3",
+    "url": "/static/js/main.26a02326.chunk.js"
+  },
+  {
+    "revision": "aba9282f90c64bc6b84d",
+    "url": "/static/js/2.ac97c6da.chunk.js"
+  },
+  {
+    "revision": "5b0434e6d789f6cf73a3",
+    "url": "/static/css/main.1e32aba6.chunk.css"
+  },
+  {
+    "revision": "aba9282f90c64bc6b84d",
+    "url": "/static/css/2.8ba914ae.chunk.css"
+  },
+  {
+    "revision": "bc790ab1124e5bb4c2234d007bc87d48",
+    "url": "/index.html"
+  }
+];
