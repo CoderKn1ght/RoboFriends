263612d63810b4561eaf0e05df43e9dd93840b6c
Shashank Gaikaiwari <shashank.gaikaiwari@gmail.com>
2019-04-03 15:50:52 -0500

    Cleaned App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,24 +21,24 @@ class App extends Component {
         this.setState({searchField: event.target.value});
     }
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        const { robots, searchField } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
-        if(this.state.robots.length === 0) {
-            return <h1>Loading</h1>
-        }
-        else {
-            return (
-                <div className='tc'>
-                    <h1>Robo Friends</h1>
-                    <SearchBox searchChange={ this.onSearchChange }/>
-                    <Scroll>
-                        <CardList robots={ filteredRobots }/>
-                    </Scroll>
-                </div>
+        return !robots.length ?  <h1>Loading</h1>
+        :
+            (
+                    <div className='tc'>
+                        <h1>Robo Friends</h1>
+                        <SearchBox searchChange={ this.onSearchChange }/>
+                        <Scroll>
+                            <CardList robots={ filteredRobots }/>
+                        </Scroll>
+                    </div>
             );
-        }
+        
     }
 }
+
 
 export default App;
